aa937e41071c6c72b684cfa1bca653f9261b7334
Jeffery Ansah <jeffery.ansah@bhp.com>
2020-04-26 07:51:09 +1000

    Updated password locker

--- Chapter_6_Manipulating_Strings/pw.py
+++ Chapter_6_Manipulating_Strings/pw.py
@@ -1,7 +1,21 @@
 #!
 #pw.py This is a password manager app
 
-PASSWORDS = {'email': 'vanslerry@gmail.com',
+PASSWORDS = {'email': 'fegpeor349jvfk/feit3n04vmwjm',
              'password': 'JVSDkjskljgeirjkgnvslkfls',
              'luggage': '124534'
              }
+
+import sys, pyperclip
+
+if len(sys.argv) < 2:
+    print('Usage: python pw.py [account] - copy account password')
+
+account = sys.argv[1] # command line argument for the account name
+
+
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print('Password for ' + account + ' copied to clipboard')
+else:
+    print('There is not account name ' + account)
